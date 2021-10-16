1cf2dc825c0291e061c7a2b1311ff5c543967b58
reckafella <ethanmuthoniw6@gmail.com>
2021-10-16 20:22:59 +0300

    house keeping

--- js/temperature.js
+++ js/temperature.js
@@ -2,9 +2,21 @@
     this file contains code for converting temperature between Celsius and Fahrenheit
 */
 // User input in Fahrenheit
-var temperature = parseFloat(prompt("Enter temperature in Fahrenheit: "));
+/* var temperature = parseFloat(
+    prompt("Enter temperature in Fahrenheit: ")
+    );
 
 var celsiusConverter = function(temperature){
     return (temperature / 33.8);
 }
-celsiusConverter();
+celsiusConverter(); */
+
+var CelsiusTemperature =  parseFloat(
+    prompt("Enter Value in Degrees Celsius: ")
+    );
+
+var FahrenheitConverter = function(CelsiusTemperature) {
+    result = (CelsiusTemperature * 1.8 + 32);
+    return result;
+}
+alert("The result is " + FahrenheitConverter(CelsiusTemperature));

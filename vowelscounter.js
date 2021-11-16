fb7d73f15de48b816251b7cb95c967e3c73804b0
reckafella <ethanmuthoniw6@gmail.com>
2021-11-16 22:43:07 +0300

    added code to count number of vowels regardless of case and presence of non-alphabet characters

--- vowelscounter.js
+++ vowelscounter.js
@@ -1,11 +1,43 @@
-function countVowel(str) {
+/* function countVowel(string) {
     // find the count of vowels
-    const count = str.toLowercase().match(/[aeiou]/gi).length;
+    const count = string.match(/[aeiou]/gi).length;
 
     //return the number of vowels
     return count;
 }
 
-let string = "a";
+let string = "a".toLowerCase;
 
 console.log(countVowel(string));
+
+ */
+
+
+// program to count number of vowels in a sentence string
+
+// define vowels
+
+const vowels = ["a", "e", "i", "o" , "u"];
+
+function countVowel(str) {
+    // initilize vowel count
+
+    let count = 0;
+
+    // loop through the string to test if each character is a vowel
+    // if true, the count is increased by 1 for every vowel found.
+
+    for (let letter of str.toLowerCase()) {
+        if(vowels.includes(letter)) {
+            count++;
+        }
+    }
+    // return the number of vowels
+    return count;
+}
+
+const string = "CATS CATERED THE EVENT";
+const strign = "CaTs CateReD tHe EveNT";
+
+console.log(countVowel(string));
+console.log(countVowel(strign));

f6f5784ef3e5bb0332e75e2042dea8e161fee884
reckafella <ethanmuthoniw6@gmail.com>
2021-11-16 23:39:59 +0300

    removed bugs and rewrote code to allow for complex cases

--- js/leapyearchecker.js
+++ js/leapyearchecker.js
@@ -1,16 +1,17 @@
 function checkLeapYear(value) {
     // check whether the year is not leap (if indivisible by 4)
-    if(value % 4 !== 0) {
+    if(value % 4 !== 0 ) {
         return false;
-    } else if(value % 4 === 0){
-        if (value % 100 === 0) {
-            return false;
-        } else if(value % 100 !== 0){
-            return true;
-        }
+    } else if(value % 400 === 0){
+        return true;
+    } else if (value % 400 !== 0 && value % 100 === 0) {
+        return false;
+    } else if(value % 4 ===0 && value % 100 !== 0) {
+        return true;
     }
 }
 console.log(checkLeapYear(1993));
 console.log(checkLeapYear(2004));
 console.log(checkLeapYear(1900));
 console.log(checkLeapYear(2000));
+console.log(checkLeapYear(2020));
